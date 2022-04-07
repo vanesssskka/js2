cde2b53b06e0a70e46fccbce67e36032116f349a
vanesssskka <mambetova2004vanessa@gmail.com>
2022-04-07 15:55:54 +0500

    init

---
+++ script.js
@@ -0,0 +1,47 @@
+const num = "18"
+console.log(num)
+
+const a = 10
+const b = 5
+const c = 2
+console.log(a + b + c)
+
+const d = 100
+console.log(d*2)
+
+const e = 67
+console.log(67%7)
+
+const f = 10
+const g = 2
+console.log( f + g)
+console.log( f - g)
+console.log( f * g)
+console.log( f % g)
+
+const h = 15
+const i = 2
+const result = h + i
+console.log(result)
+
+const j = 30
+const k = 15
+const l = j + k
+console.log(l%4)
+
+const n = 17
+const o = 10
+const p = n - o
+const q = 7
+const calc = p + q
+console.log(calc)
+
+const m = "10"
+const r = "99"
+const addition = m + r
+console.log(addition)
+
+const name = "лет "
+const number = "10"
+const namenumber = name + number
+console.log(namenumber)
